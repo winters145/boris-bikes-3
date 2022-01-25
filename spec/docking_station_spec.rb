ee67a6fd4ccc6b620e6a641356da84c551cb280b
Claire Nelson <claire.nelson@johnlewis.co.uk>
2022-01-25 18:03:54 +0000

    Add release_bike test and method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
 require "docking_station"
 describe DockingStation do 
-
-
+    describe "#release_bike" do
+    # it "should respond to release_bike" do
+    #     docking_station = DockingStation.new
+    #     expect(docking_station).to respond_to(:release_bike)
+    # end
+    it { expect(DockingStation.new).to respond_to(:release_bike) }
+  end
 end 
