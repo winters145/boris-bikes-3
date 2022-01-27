9e9fc3773dc5af512d8d0b746a541ecac95e098c
Jack <winterschladen.j@gmail.com>
2022-01-27 16:39:34 +0000

    Completed section 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,15 +9,17 @@ class DockingStation
     end 
 
     def release_bike 
-      raise "no bikes" if @stored_bikes.empty? 
+      raise "no bikes" if empty?
       @stored_bikes.last
     end
 
     def dock(bike)
-      raise "station full" if full? == true
+      raise "station full" if full?
       @stored_bikes << bike
       @bike = bike
     end
+
+    private
 
     def full?
       if @stored_bikes.count < 20
@@ -26,5 +28,12 @@ class DockingStation
         true
      end
     end
-
+    
+    def empty?
+      if @stored_bikes.empty?
+        true
+      else
+        false
+      end
+    end
 end 
